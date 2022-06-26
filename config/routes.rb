3eea24491e2cd5cb7631da38c0fb4e06fae46621
Grace Joh <graceeunaejoh@gmail.com>
2022-06-26 17:09:51 -0400

    add soft delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post "/items" => "items#create"
   get "/items/:id" => "items#show"
   patch "/items/:id" => "items#update"
-  delete "/items/:id" => "items#destroy"
+  # delete "/items/:id" => "items#destroy"
+
+  post "/delete_item/:id" => "items#destroy"
 end
---
+++ db/migrate/20220626210228_add_delete_comment_to_item.rb
@@ -0,0 +1,4 @@
+class AddDeleteCommentToItem < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
