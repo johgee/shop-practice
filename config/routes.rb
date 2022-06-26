c11bde352905d8bcb8e416bda0e18108bb94c6e9
Grace Joh <graceeunaejoh@gmail.com>
2022-06-26 16:57:05 -0400

    crud app

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "/items" => "items#index"
+  post "/items" => "items#create"
+  get "/items/:id" => "items#show"
+  patch "/items/:id" => "items#update"
+  delete "/items/:id" => "items#destroy"
+end
